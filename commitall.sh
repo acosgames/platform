00d3e8a9401d59f146f7549d7acc5ae2498262f8
joelruiz2 <joelruiz2@gmail.com>
2021-01-31 09:45:54 -0600

    test

--- commitall.sh
+++ commitall.sh
@@ -1,33 +1,39 @@
+echo ">>>> Committing forkoff-api"
 cd forkoff-api
 git add .
 git commit -m "$1"
 git push
 cd ..
 
+echo ">>>> Committing forkoff-client"
 cd forkoff-client
 git add .
 git commit -m "$1"
 git push
 cd ..
 
+echo ">>>> Committing forkoff-gameserver"
 cd forkoff-gameserver
 git add .
 git commit -m "$1"
 git push
 cd ..
 
+echo ">>>> Committing forkoff-shared"
 cd forkoff-shared
 git add .
 git commit -m "$1"
 git push
 cd ..
 
+echo ">>>> Committing forkoff-websocket"
 cd forkoff-websocket
 git add .
 git commit -m "$1"
 git push
 cd ..
 
+echo ">>>> Committing forkoff-platform"
 git add .
 git commit -m "$1"
 git push
