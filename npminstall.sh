07dadd4ceb926e312bb95b7802fd2c3c366cd6fb
joelruiz2 <joelruiz2@gmail.com>
2021-01-31 20:48:34 -0600

    ran npm install

---
+++ npminstall.sh
@@ -0,0 +1,33 @@
+# Optional branch for 1st argument
+# Last argument is always the commit message
+
+
+echo "=========================================================="
+echo ">>>> NPM INSTALL forkoff-api"
+cd forkoff-api
+npm install
+cd ..
+echo "=========================================================="
+echo ">>>> NPM INSTALL forkoff-client"
+cd forkoff-client
+npm install
+cd ..
+echo "=========================================================="
+echo ">>>> NPM INSTALL forkoff-gameserver"
+cd forkoff-gameserver
+npm install
+cd ..
+echo "=========================================================="
+echo ">>>> NPM INSTALL forkoff-shared"
+cd forkoff-shared
+npm install
+cd ..
+echo "=========================================================="
+echo ">>>> NPM INSTALL forkoff-websocket"
+cd forkoff-websocket
+npm install
+cd ..
+echo "=========================================================="
+echo ">>>> NPM INSTALL forkoff-platform"
+npm install
+echo "=========================================================="
