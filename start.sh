d004d92fe8aaa0324d68472138363d9391608fd5
joelruiz2 <joelruiz2@gmail.com>
2021-06-05 23:52:35 -0500

    made platform more resilient, still needs more, also working on poptrivia test game

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
- 
+#!/bin/bash
+
+docker run -d -p 5672:5672 --name rabbitmq -e RABBITMQ_DEFAULT_USER=fsg -e RABBITMQ_DEFAULT_PASS=haha123hehe rabbitmq:3
+
+docker run -d -p 6379:6379 --name redis redis
