df48d0f1a3c08b9d3ff75dabdaa8e4b54e966d82
joelruiz2 <joelruiz2@gmail.com>
2021-11-24 01:54:46 -0600

    fixed resilient gameserver, added game errors to sql

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run -d -p 5672:5672 --name rabbitmq -e RABBITMQ_DEFAULT_USER=fsg -e RABBITMQ_DEFAULT_PASS=haha123hehe rabbitmq:3
+docker run -d -p 5672:5672 -p 15672:15672 --name rabbitmq -e RABBITMQ_DEFAULT_USER=fsg -e RABBITMQ_DEFAULT_PASS=haha123hehe rabbitmq
 
 docker run -d -p 6379:6379 --name redis redis
 
-mysqld
+# mysqld
