5eb10d32fea6ce73f2e08b60e555155a05771aa7
Joel <joelruiz2@gmail.com>
2021-12-12 02:26:44 -0600

    using Chakra UI, updating all components

--- dummy.js
+++ dummy.js
@@ -1,5 +1,45 @@
-const { genFullShortId } = require('./fsg-shared/util/idgen');
+// const { genFullShortId } = require('./fsg-shared/util/idgen');
 
-for(var i=0; i<5; i++) {
-    console.log(genFullShortId(5));
+// for(var i=0; i<5; i++) {
+//     console.log(genFullShortId(5));
+// }
+
+
+const redis = require('./fsg-shared/services/redis');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function run() {
+
+
+    while (!redis.isActive) {
+
+        console.warn("[WebSocket] waiting on redis...");
+        await this.sleep(1000);
+        //return;
+    }
+
+    await redis.connect();
+
+    let json = {
+        timer: 999,
+        seq: 3
+    }
+    await redis.hset('action-1', '1234', JSON.stringify(json))
+
+    let json2 = {
+        timer: 444,
+        seq: 6
+    }
+    await redis.hset('action-1', 'abcd', JSON.stringify(json2));
+
+    let result = await redis.hget('action-1', '1234');
+    console.log(result);
+
+    let result2 = await redis.hgetall('action-1');
+    console.log(result2);
+}
+
+run();
