662271ecdb988abc4a3b4eb416ba817ed704c73e
A Cup of Skill <admin@acos.games>
2022-04-18 13:18:21 -0500

    fixing redis not having a password

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 docker run -d -p 5672:5672 -p 15672:15672 --name rabbitmq -e RABBITMQ_DEFAULT_USER=acosg -e RABBITMQ_DEFAULT_PASS=haha123hehe rabbitmq
 
-docker run -d -p 6379:6379 --name redis redis
+docker run -d -p 6379:6379 --name redis redis --requirepass "haha123hehe"
 
 docker exec -it redis redis-cli FLUSHALL
 
